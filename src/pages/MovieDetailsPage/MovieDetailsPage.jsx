1bc465f55468cd21159a00278c2af23304e20cab
Esnobart <offrichterrr@gmail.com>
2024-02-25 14:48:39 +0000

    update

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,13 +3,17 @@ import { NavLink, Link, Outlet, useParams, useLocation } from 'react-router-dom'
 import css from './MovieDetailsPage.module.css';
 import { fetchFilm } from '../../api';
 import Loading from '../../Components/Loading/Loading';
+import { useRef } from 'react';
 
 export default function MovieDetailsPage() {
     const location = useLocation();
     const { id } = useParams();
     const [loading, setLoading] = useState(false);
     const [movie, setMovie] = useState(null);
-    const imgUrl = "https://image.tmdb.org/t/p/w500";
+    const goBack = useRef(location?.state ?? '/')
+    const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
+
+
 
     useEffect(() => {
         async function fetchMovie() {
@@ -27,11 +31,11 @@ export default function MovieDetailsPage() {
     return (
         <div>
             { loading && <Loading />}
-            <NavLink to={location.state}>Go Back</NavLink>
+            <NavLink to={goBack.current}>Go Back</NavLink>
             {movie && (
                 <div>
                 <div className={css.detailsContainer}>
-                    <div className={css.imageContainer}><img src={imgUrl + movie.poster_path} /></div>
+                    <div className={css.imageContainer}><img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`  : defaultImg} width={250} alt="poster" /></div>
                     <div className={css.infoContainer}>
                         <h1>{movie.title}</h1>
                         <p>User score: {movie.vote_average > 0 ? ((movie.vote_average / 10) * 100).toFixed(2) + '%' : 'N/A' }</p>
